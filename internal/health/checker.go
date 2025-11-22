baecd4e5675bdda6a781f9859810f734e995dc86
Johnny2705 <johnny.grigore@unicorn-tech.org>
2025-11-22 22:27:16 +0200

    Added health checks

--- internal/health/checker.go
+++ internal/health/checker.go
@@ -1,1 +1,21 @@
 package health
+
+import "github.com/Johnny2705/go-tunnel-gateway/internal/config"
+
+type Checker struct {
+	cfg *config.Config
+}
+
+func NewChecker(cfg *config.Config) *Checker {
+	return &Checker{
+		cfg: cfg,
+	}
+}
+
+func (c *Checker) CheckLiveness() error {
+	return nil
+}
+
+func (c *Checker) CheckReadiness() error {
+	return nil
+}
--- internal/httapi/health.go
+++ internal/httapi/health.go
@@ -1,1 +1,43 @@
-package httapi
+package httpapi
+
+import (
+	"net/http"
+
+	"github.com/Johnny2705/go-tunnel-gateway/internal/health"
+)
+
+type HealthHandler struct {
+	checker *health.Checker
+}
+
+func NewHealthHandler(checker *health.Checker) *HealthHandler {
+	return &HealthHandler{
+		checker: checker,
+	}
+}
+
+func (h *HealthHandler) writeStatus(w http.ResponseWriter, status int, msg string) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(status)
+	w.Write([]byte(msg))
+}
+
+func (h *HealthHandler) Liveness(w http.ResponseWriter, r *http.Request) {
+	err := h.checker.CheckLiveness()
+
+	if err != nil {
+		h.writeStatus(w, 500, "unhealthy")
+		return
+	}
+	h.writeStatus(w, 200, "ok")
+}
+
+func (h *HealthHandler) Readiness(w http.ResponseWriter, r *http.Request) {
+	err := h.checker.CheckReadiness()
+
+	if err != nil {
+		h.writeStatus(w, 500, "unhealthy")
+		return
+	}
+	h.writeStatus(w, 200, "ok")
+}
